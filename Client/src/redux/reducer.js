d93e30261a69bf3816dd33df5b25b0d7c2f10c90
Juan Pablo Accinelli <juanpaccinelli@gmail.com>
2023-07-13 21:32:32 -0300

    Solucionar errores del reducer

--- Client/src/redux/reducer.js
+++ Client/src/redux/reducer.js
@@ -2,8 +2,7 @@ import { ADD_FAV, REMOVE_FAV, FILTER, ORDER } from './actions/action_types';
 
 const initialState = {
   myFavorites: [], //? --> es lo que MOSTRAMOS en pantalla y donde filtramos y ordenamos los characters
-  allCharacters: [], //? --> es la copia de seguridad que NO se modifica 
- 
+  allCharacters: [], //? --> es la copia de seguridad que NO se modifica
 };
 
 const reducer = (state = initialState, action) => {
@@ -16,7 +15,9 @@ const reducer = (state = initialState, action) => {
       };
 
     case REMOVE_FAV:
-      let copy = state.myFavorites.filter((character) => parseInt(character.id) !== parseInt(action.payload, 10));
+      let copy = state.myFavorites.filter(
+        (character) => parseInt(character.id) !== parseInt(action.payload, 10)
+      );
       return {
         ...state,
         myFavorites: copy,
@@ -24,28 +25,52 @@ const reducer = (state = initialState, action) => {
       };
 
     case FILTER:
-      // const allCharactersFiltered = state.allCharacters.filter(
+      // // const allCharactersFiltered = state.allCharacters.filter(
+      // //   (character) => character.gender === action.payload
+      // // );
+      // let copy2 = [...state.allCharacters];
+      // let allCharactersFiltered = copy2.filter(
       //   (character) => character.gender === action.payload
       // );
-      let copy2 = [...state.allCharacters];
-      let allCharactersFiltered = copy2.filter(
-        (character) => character.gender === action.payload
-      );
+      // return {
+      //   ...state,
+      //   myFavorites:
+      //     action.payload === 'All' ? [...state.allCharacters] : allCharactersFiltered,
+      // };
+      let allCharactersFiltered = [];
+
+      if (action.payload === 'All') {
+        allCharactersFiltered = [...state.allCharacters];
+      } else {
+        allCharactersFiltered = state.allCharacters.filter(
+          (character) => character.gender === action.payload
+        );
+      }
+
       return {
         ...state,
-        myFavorites:
-          action.payload === 'All' ? [...state.allCharacters] : allCharactersFiltered,
+        myFavorites: allCharactersFiltered,
       };
 
     case ORDER:
-      const allCharactersCopy = [...state.allCharacters];
+      // const allCharactersCopy = [...state.allCharacters];
+      // return {
+      //   ...state,
+      //   myFavorites:
+      //     action.payload === 'A'
+      //       ? allCharactersCopy.sort((a, b) => a.id - b.id)
+      //       : allCharactersCopy.sort((a, b) => b.id - a.id),
+      // };
+      const myFavoritesCopy = [...state.myFavorites];
+
       return {
         ...state,
         myFavorites:
           action.payload === 'A'
-            ? allCharactersCopy.sort((a, b) => a.id - b.id)
-            : allCharactersCopy.sort((a, b) => b.id - a.id),
+            ? myFavoritesCopy.sort((a, b) => a.id - b.id)
+            : myFavoritesCopy.sort((a, b) => b.id - a.id),
       };
+
     default:
       return { ...state };
   }
