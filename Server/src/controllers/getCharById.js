a8b7d2e7b58680d46db4764522f37f5d85998989
Juan Pablo Accinelli <juanpaccinelli@gmail.com>
2023-08-01 17:41:24 -0300

    Concluye la HW de ASYNC/AWAIT exitosamente

--- Server/src/controllers/getCharById.js
+++ Server/src/controllers/getCharById.js
@@ -1,23 +1,49 @@
 const axios = require('axios');
 const URL = 'https://rickandmortyapi.com/api/character';
 
-const getCharById = (req, res) => {
+//* --> solución PREVIA a la HW de ASYNC/AWAIT
+// const getCharById = (req, res) => {
+//   const { id } = req.params;
+
+//   axios(`${URL}/${id}`)
+//     .then((response) => response.data)
+//     .then(({ status, name, species, origin, image, gender }) => {
+
+//       if (name) { //* evalúo NAME (o CUALQUIERA de las otras propiedades) menos ID porque ID SIEMPRE va ser true (no importa que devuelva o no un)
+//         const character = { id, status, name, species, origin, image, gender };
+//         return res.status(200).json(character); //* si NO le hubiera indicado el STATUS, por default, devolvería 200
+//       }
+
+//       return res.status(404).send('Not found'); //* es BUENA PRÁCTICA evitar el ELSE y, en su lugar, colocar un RETURN para que CORTE la ejecución
+//     })
+//     .catch((error) => {
+//       return res.status(500).send(error.message)
+//     });
+// };
+
+const getCharById = async (req, res) => {
   const { id } = req.params;
 
-  axios(`${URL}/${id}`)
-    .then((response) => response.data)
-    .then(({ status, name, species, origin, image, gender }) => {
+  try {
+    const { data } = await axios(`${URL}/${id}`);
 
-      if (name) { //* evalúo NAME (o CUALQUIERA de las otras propiedades) menos ID porque ID SIEMPRE va ser true (no importa que devuelva o no un)
-        const character = { id, status, name, species, origin, image, gender };
-        return res.status(200).json(character); //* si NO le hubiera indicado el STATUS, por default, devolvería 200
-      }
+    const {
+      status,
+      name,
+      species,
+      origin,
+      image,
+      gender,
+    } = data;
 
-      return res.status(404).send('Not found'); //* es BUENA PRÁCTICA evitar el ELSE y, en su lugar, colocar un RETURN para que CORTE la ejecución
-    })
-    .catch((error) => {
-      return res.status(500).send(error.message)
-    });
+    const character = { id, status, name, species, origin, image, gender };
+
+    return name
+      ? res.status(200).json(character)
+      : res.status(404).send('Not found');
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
 };
 
 module.exports = getCharById;
